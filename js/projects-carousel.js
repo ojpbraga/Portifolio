2efe42386bba69ff5ede4d24f4f191364cdac83b
ojpbraga <ojpbraga@gmail.com>
2023-12-26 20:37:29 -0300

    feat: estruturação

---
+++ js/projects-carousel.js
@@ -0,0 +1,14 @@
+const itemsCarousel = document.querySelectorAll('.carousel-item');
+const containerCarousel = document.querySelector('#div-carousel-container');
+
+
+   
+containerCarousel.addEventListener('scroll', (e) => {
+    if(scrollY => 0) {
+        itemsCarousel.forEach((el, index) => {
+            // console.log(el.scroll, index)
+        })
+    }
+        
+});
+
