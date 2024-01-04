bfbc2973d240190886ca9b007393fff1731ca558
ojpbraga <ojpbraga@gmail.com>
2024-01-04 19:41:35 -0300

    feat: carousel

--- js/projects-carousel.js
+++ js/projects-carousel.js
@@ -1,14 +1,43 @@
 const itemsCarousel = document.querySelectorAll('.carousel-item');
 const containerCarousel = document.querySelector('#div-carousel-container');
+const sectionProjects = document.querySelector('#projects');
 
-
-   
-containerCarousel.addEventListener('scroll', (e) => {
-    if(scrollY => 0) {
-        itemsCarousel.forEach((el, index) => {
-            // console.log(el.scroll, index)
-        })
-    }
+// Ative quando a página estiver no topo 
+function carouselAnimation() {
+    function animationDiv() {
+        itemsCarousel.forEach((div, index) => {
+            const divTop = div.getBoundingClientRect().top;
+    
+            const mainDiv = div.nextElementSibling;
         
-});
+            setTimeout(() => {
+                
+                if(divTop <= 0) {
+                    itemsCarousel.forEach((div) => {
+                        div.classList.remove('active');
+                    });
 
+                    
+                    div.classList.add('active');
+                    div.nextElementSibling.nextElementSibling.classList.add('active');
+    
+                    
+                    // Resolver o 100, pois dependendo da tela esse valor mudar
+        
+                } else if(divTop <= 100) {
+                    div.nextElementSibling.classList.add('active');
+                    itemsCarousel[0].classList.remove('active');
+                    
+                }
+            }, 800);
+            
+    
+        });
+    }
+    containerCarousel.addEventListener('scroll', animationDiv);
+    
+}
+
+if(sectionProjects.getBoundingClientRect().top >= 0) {
+    carouselAnimation();
+}
