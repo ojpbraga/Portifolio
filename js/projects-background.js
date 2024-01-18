67004dfd67fb416c8413f42b9104b75c6b4522cc
ojpbraga <ojpbraga@gmail.com>
2024-01-18 20:45:28 -0300

    feat: & fix: dark mode

--- js/projects-background.js
+++ js/projects-background.js
@@ -1,149 +1,151 @@
 
-// const sectionProjects = document.querySelector('#projects');
-const formatsProjects = document.querySelectorAll('.formats');
+// // ATENÇÃO: colocar dentro de uma função que é chamada apenas quando estiver na tela de Projects para melhor peformance
 
-const circleProjects = document.querySelector('#circle');
-const squareProjects = document.querySelector('#square');
-const rectangleProjects = document.querySelector('#rectangle');
-const starProjects = document.querySelector('#star');
-const triangleProjects = document.querySelector('#triangle');
-const diamondProjects = document.querySelector('#diamond');
-const elipseProjects = document.querySelector('#elipse');
+// // const sectionProjects = document.querySelector('#projects');
+// const formatsProjects = document.querySelectorAll('.formats');
 
-// Circle
-let xMoving = 0;
-let yMoving = 0;
-let xSpeed = 1;
-let ySpeed = 1;
-const fps = 90;
+// const circleProjects = document.querySelector('#circle');
+// const squareProjects = document.querySelector('#square');
+// const rectangleProjects = document.querySelector('#rectangle');
+// const starProjects = document.querySelector('#star');
+// const triangleProjects = document.querySelector('#triangle');
+// const diamondProjects = document.querySelector('#diamond');
+// const elipseProjects = document.querySelector('#elipse');
 
-function update(element, xPosition, yPosition) {
-    if(40 + xPosition + xMoving + element.clientWidth >= window.innerWidth || xPosition + xMoving <= 0) {
-        xSpeed = -xSpeed;
-    }
-    if(40 + yPosition + yMoving + element.clientHeight >= window.innerHeight || yPosition + yMoving <= 0) {
-        ySpeed = -ySpeed;
-    }
+// // Circle
+// let xMoving = 0;
+// let yMoving = 0;
+// let xSpeed = 1;
+// let ySpeed = 1;
+// const fps = 90;
 
-    element.style.left = xPosition + xMoving + "px";
-    element.style.top = yPosition + yMoving + "px";
-}
+// function update(element, xPosition, yPosition) {
+//     if(40 + xPosition + xMoving + element.clientWidth >= window.innerWidth || xPosition + xMoving <= 0) {
+//         xSpeed = -xSpeed;
+//     }
+//     if(40 + yPosition + yMoving + element.clientHeight >= window.innerHeight || yPosition + yMoving <= 0) {
+//         ySpeed = -ySpeed;
+//     }
 
-update(circleProjects, 34, 254);
+//     element.style.left = xPosition + xMoving + "px";
+//     element.style.top = yPosition + yMoving + "px";
+// }
 
-setInterval(() => {
-    xMoving += xSpeed;
-    yMoving += ySpeed;
+// update(circleProjects, 34, 254);
 
-
-    update(circleProjects, 34, 454);
-
-}, 1000/fps);
-
-// Square
-let xMoving2 = 0;
-let yMoving2 = 0;
-let xSpeed2 = 1;
-let ySpeed2 = 1;
-
-function update2(element, xPosition2, yPosition2) {
-    if(40 + xPosition2 + xMoving2 + element.clientWidth >= window.innerWidth || xPosition2 + xMoving2 <= 0) {
-        xSpeed2 = -xSpeed2;
-    }
-    if(40 + yPosition2 + yMoving2 + element.clientHeight >= window.innerHeight || yPosition2 + yMoving2 <= 0) {
-        ySpeed2 = -ySpeed2;
-    }
-
-    element.style.left = xPosition2 + xMoving2 + "px";
-    element.style.top = yPosition2 + yMoving2 + "px";
-}
-
-setInterval(() => {
-    xMoving2 += xSpeed2;
-    yMoving2 += ySpeed2;
+// setInterval(() => {
+//     xMoving += xSpeed;
+//     yMoving += ySpeed;
 
 
-    update2(squareProjects, 324, 54);
+//     update(circleProjects, 34, 454);
 
-}, 1000/fps);
+// }, 1000/fps);
 
-// Rectangle
-let xMoving3 = 0;
-let yMoving3 = 0;
-let xSpeed3 = 1;
-let ySpeed3 = 1;
+// // Square
+// let xMoving2 = 0;
+// let yMoving2 = 0;
+// let xSpeed2 = 1;
+// let ySpeed2 = 1;
 
-function update3(element, xPosition3, yPosition3) {
-    if(40 + xPosition3 + xMoving3 + element.clientWidth >= window.innerWidth || xPosition3 + xMoving3 <= 0) {
-        xSpeed3 = -xSpeed3;
-    }
-    if(40 + yPosition3 + yMoving3 + element.clientHeight >= window.innerHeight || yPosition3 + yMoving3 <= 0) {
-        ySpeed3 = -ySpeed3;
-    }
+// function update2(element, xPosition2, yPosition2) {
+//     if(40 + xPosition2 + xMoving2 + element.clientWidth >= window.innerWidth || xPosition2 + xMoving2 <= 0) {
+//         xSpeed2 = -xSpeed2;
+//     }
+//     if(40 + yPosition2 + yMoving2 + element.clientHeight >= window.innerHeight || yPosition2 + yMoving2 <= 0) {
+//         ySpeed2 = -ySpeed2;
+//     }
 
-    element.style.left = xPosition3 + xMoving3 + "px";
-    element.style.top = yPosition3 + yMoving3 + "px";
-}
+//     element.style.left = xPosition2 + xMoving2 + "px";
+//     element.style.top = yPosition2 + yMoving2 + "px";
+// }
 
-setInterval(() => {
-    xMoving3 += xSpeed3;
-    yMoving3 += ySpeed3;
-
-
-    update3(rectangleProjects, 24, 354);
-
-}, 1000/fps);
-
-// Star
-let xMoving4 = 0;
-let yMoving4 = 0;
-let xSpeed4 = 1;
-let ySpeed4 = 1;
-
-function update4(element, xPosition4, yPosition4) {
-    if(40 + xPosition4 + xMoving4 + element.clientWidth >= window.innerWidth || xPosition4 + xMoving4 <= 0) {
-        xSpeed4 = -xSpeed4;
-    }
-    if(40 + yPosition4 + yMoving4 + element.clientHeight >= window.innerHeight || yPosition4 + yMoving4 <= 0) {
-        ySpeed4 = -ySpeed4;
-    }
-
-    element.style.left = xPosition4 + xMoving4 + "px";
-    element.style.top = yPosition4 + yMoving4 + "px";
-}
-
-setInterval(() => {
-    xMoving4 += xSpeed4;
-    yMoving4 += ySpeed4;
+// setInterval(() => {
+//     xMoving2 += xSpeed2;
+//     yMoving2 += ySpeed2;
 
 
-    update4(starProjects, 300, 400);
+//     update2(squareProjects, 324, 54);
 
-}, 1000/fps);
+// }, 1000/fps);
 
-// Elipse
-let xMoving5 = 0;
-let yMoving5 = 0;
-let xSpeed5 = 1;
-let ySpeed5 = 1;
+// // Rectangle
+// let xMoving3 = 0;
+// let yMoving3 = 0;
+// let xSpeed3 = 1;
+// let ySpeed3 = 1;
 
-function update5(element, xPosition5, yPosition5) {
-    if(40 + xPosition5 + xMoving5 + element.clientWidth >= window.innerWidth || xPosition5 + xMoving5 <= 0) {
-        xSpeed5 = -xSpeed5;
-    }
-    if(40 + yPosition5 + yMoving5 + element.clientHeight >= window.innerHeight || yPosition5 + yMoving5 <= 0) {
-        ySpeed5 = -ySpeed5;
-    }
+// function update3(element, xPosition3, yPosition3) {
+//     if(40 + xPosition3 + xMoving3 + element.clientWidth >= window.innerWidth || xPosition3 + xMoving3 <= 0) {
+//         xSpeed3 = -xSpeed3;
+//     }
+//     if(40 + yPosition3 + yMoving3 + element.clientHeight >= window.innerHeight || yPosition3 + yMoving3 <= 0) {
+//         ySpeed3 = -ySpeed3;
+//     }
 
-    element.style.left = xPosition5 + xMoving5 + "px";
-    element.style.top = yPosition5 + yMoving5 + "px";
-}
+//     element.style.left = xPosition3 + xMoving3 + "px";
+//     element.style.top = yPosition3 + yMoving3 + "px";
+// }
 
-setInterval(() => {
-    xMoving5 += xSpeed5;
-    yMoving5 += ySpeed5;
+// setInterval(() => {
+//     xMoving3 += xSpeed3;
+//     yMoving3 += ySpeed3;
 
 
-    update5(elipseProjects, 10, 400);
+//     update3(rectangleProjects, 24, 354);
 
-}, 1000/fps);
+// }, 1000/fps);
+
+// // Star
+// let xMoving4 = 0;
+// let yMoving4 = 0;
+// let xSpeed4 = 1;
+// let ySpeed4 = 1;
+
+// function update4(element, xPosition4, yPosition4) {
+//     if(40 + xPosition4 + xMoving4 + element.clientWidth >= window.innerWidth || xPosition4 + xMoving4 <= 0) {
+//         xSpeed4 = -xSpeed4;
+//     }
+//     if(40 + yPosition4 + yMoving4 + element.clientHeight >= window.innerHeight || yPosition4 + yMoving4 <= 0) {
+//         ySpeed4 = -ySpeed4;
+//     }
+
+//     element.style.left = xPosition4 + xMoving4 + "px";
+//     element.style.top = yPosition4 + yMoving4 + "px";
+// }
+
+// setInterval(() => {
+//     xMoving4 += xSpeed4;
+//     yMoving4 += ySpeed4;
+
+
+//     update4(starProjects, 300, 400);
+
+// }, 1000/fps);
+
+// // Elipse
+// let xMoving5 = 0;
+// let yMoving5 = 0;
+// let xSpeed5 = 1;
+// let ySpeed5 = 1;
+
+// function update5(element, xPosition5, yPosition5) {
+//     if(40 + xPosition5 + xMoving5 + element.clientWidth >= window.innerWidth || xPosition5 + xMoving5 <= 0) {
+//         xSpeed5 = -xSpeed5;
+//     }
+//     if(40 + yPosition5 + yMoving5 + element.clientHeight >= window.innerHeight || yPosition5 + yMoving5 <= 0) {
+//         ySpeed5 = -ySpeed5;
+//     }
+
+//     element.style.left = xPosition5 + xMoving5 + "px";
+//     element.style.top = yPosition5 + yMoving5 + "px";
+// }
+
+// setInterval(() => {
+//     xMoving5 += xSpeed5;
+//     yMoving5 += ySpeed5;
+
+
+//     update5(elipseProjects, 10, 400);
+
+// }, 1000/fps);
--- js/script.js
+++ js/script.js
@@ -94,14 +94,22 @@ const html = document.querySelector("html");
 const buttonTheme = document.querySelector("#button-switch-theme");
 
 buttonTheme.addEventListener('click', () => {
+    // Troca das cores
     html.classList.toggle('dark-mode');
 
-    // Verificando qual tema está para troca de cor do header
     if(html.classList.contains("dark-mode")) {
+        // Verificando qual tema está para troca de cor do header
         changeHeader("linear-gradient(90deg, rgba(64, 92, 217, 0.70) 0%, rgba(167, 85, 218, 0.32) 33.44%, rgba(228, 50, 91, 0.49) 64.17%, rgba(242, 205, 110, 0.49) 88.65%)");
-        console.log('Dark')
+
+        // Troca de icon button
+        buttonTheme.setAttribute('src', '../assets/img/sun-solid_light.svg');
+
     } else {
+        // Verificando qual tema está para troca de cor do header
         changeHeader("linear-gradient(90deg, #405CD9 0%, rgba(167, 85, 218, 0.8) 33.44%, rgba(228, 50, 91, 0.8) 64.17%, rgba(242, 205, 110, 0.8) 88.65%)");
-        console.log('Light')
+    
+        // Troca de icon button
+        buttonTheme.setAttribute('src', '../assets/img/moon.svg');
+
     }
 })
